9d4cbe12ff0eaaab22c6078e2d0b8913ec9eb604
Isaac <isb14683@gmail.com>
2025-01-20 14:46:44 +1000

    Single Character Tokens

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,11 +1,10 @@
+
 use crate::RedbellyError;
-
-
 
 #[rustfmt::skip]
 #[allow(dead_code)]
-#[derive(Debug, Clone)]
-enum TokenType {
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
     // Single-character tokens.
   LeftParen, RightParen, LeftBrace, RightBrace,
   Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
@@ -26,44 +25,79 @@ enum TokenType {
   Eof
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
-    line: usize,
 }
 
 impl Token {
-    fn new(token_type: TokenType, lexeme: String, line: usize) -> Self {
+    fn from_str(token_type: TokenType, lexeme: &str) -> Self {
         Self {
             token_type,
-            lexeme,
-            line,
+            lexeme: String::from(lexeme),
         }
     }
-    // Using debug print here is a bit stinky
-    fn to_string(&self) -> String {
-        return format!("{:?} {}", self.token_type, self.lexeme);
+    fn new(token_type: TokenType, lexeme: String) -> Self {
+        Self {
+            token_type,
+            lexeme: String::from(lexeme),
+        }
     }
 }
 
 pub struct Lexer {
-    source: Vec<u8>,
+    source: Vec<char>,
     tokens: Vec<Token>,
+    index: usize,
+    line: usize,
+    start: usize,
 }
 
 impl Lexer {
+    pub fn new(source: String) -> Self {
+        Self {
+            source: source.chars().collect(),
+            tokens: vec![],
+            index: 0,
+            line: 0,
+            start: 0,
+        }
+    }
 
-    pub fn new() {
-        todo!();
+    pub fn scan(&mut self) -> Result<Vec<Token>, RedbellyError> {
+        while !self.is_at_end() {
+            self.start = self.index;
+            match self.scan_token() {
+                Ok(_) => (),
+                Err(error) => return Err(error),
+            }
+        }
+
+        self.tokens
+            .push(Token::from_str(TokenType::Eof, ""));
+
+        return Ok(self.tokens.clone());
+    }
+
+    fn consume(&mut self) -> &char {
+        let c = self.source.get(self.index).expect("Out of Bounds");
+        self.index += 1;
+        return c;
+    }
+
+    fn peek(&self) -> &char {
+        return self.source.get(self.index + 1).expect("Out of Bounds");
     }
 
     fn add_token(&mut self, token_type: TokenType) -> Result<(), RedbellyError> {
-        todo!();
+        let lexeme: String = self.source[self.start..self.index].iter().clone().collect();
+        self.tokens.push(Token::new(token_type, lexeme));
+        return Ok(());
     }
 
     fn scan_token(&mut self) -> Result<(), RedbellyError> {
-        let c: char = todo!();
+        let c: &char = self.consume();
         match c {
             '(' => self.add_token(TokenType::LeftParen),
             ')' => self.add_token(TokenType::RightParen),
@@ -75,8 +109,31 @@ impl Lexer {
             '+' => self.add_token(TokenType::Plus),
             ';' => self.add_token(TokenType::Semicolon),
             '*' => self.add_token(TokenType::Star),
-            _ => return Err(RedbellyError::new("Unexpected Character", 0)),
+            _ => return Err(RedbellyError::new("Unexpected Character", self.line)),
         }
+    }
+
+    fn is_at_end(&self) -> bool {
+        return self.index >= self.source.len();
     }
 }
 
+#[cfg(test)]
+mod lexer_tests {
+    use super::*;
+
+    #[test]
+    fn test_single_characters() {
+        let test_tokens: Vec<Token> = vec![
+            Token::from_str(TokenType::LeftParen, "("),
+            Token::from_str(TokenType::RightParen, ")"),
+            Token::from_str(TokenType::LeftBrace, "{"),
+            Token::from_str(TokenType::RightBrace, "}"),
+            Token::from_str(TokenType::Eof, ""),
+        ];
+        let string = String::from("(){}");
+        let mut lexer = Lexer::new(string.clone());
+        let tokens = lexer.scan().unwrap();
+        assert_eq!(test_tokens, tokens);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,26 @@
 use lexer::Lexer;
 use std::{
-    fs::File,
-    io::{stdin, stdout, Read, Write},
+    fmt::{self, Display}, fs::File, io::{stdin, stdout, Read, Write}
 };
 
-fn run(contents: Vec<u8>) -> Result<(), RedbellyError> {
-    todo!()
+fn run(contents: String) {
+    let mut lexer = Lexer::new(contents);
+    let result = lexer.scan();
+    match result {
+        Ok(tokens) => {
+            for token in tokens {
+                println!("{:?}", token)
+            }
+        }
+        Err(error) => eprintln!("{:?}", error)
+    }
 }
 
 pub fn run_file(file_path: &String) {
-    let mut contents: Vec<u8> = vec![];
+    let mut contents = String::new();
     let _ = File::open(file_path)
         .expect("Invalid File Path")
-        .read_to_end(&mut contents)
+        .read_to_string(&mut contents)
         .expect("Invalid File Contents");
     run(contents);
     // Proper exit codes one day
@@ -22,9 +30,9 @@ pub fn run_prompt() {
     loop {
         print!(">");
         let _ = stdout().flush();
-        let mut input = vec![];
-        match stdin().read_to_end(&mut input) {
-            Ok(n) => run(input).unwrap(),
+        let mut input = String::new();
+        match stdin().read_to_string(&mut input) {
+            Ok(_) => run(input),
             Err(error) => eprintln!("Error Reading Input: {}", error),
         };
     }
@@ -42,6 +50,12 @@ impl RedbellyError {
             message: message.to_string(),
             line,
         }
+    }
+}
+
+impl Display for RedbellyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[Line {}]: {}", self.line, self.message)
     }
 }
 
